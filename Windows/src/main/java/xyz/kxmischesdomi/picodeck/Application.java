2e7013ffdf483cc49b5272e6e41ea0652aa12c35
KxmischesDomi <kxmischesdomi@gmail.com>
2023-05-16 23:46:44 +0200

    Delete legacy stuff

--- Windows/src/main/java/xyz/kxmischesdomi/picodeck/Application.java
+++ Windows/src/main/java/xyz/kxmischesdomi/picodeck/Application.java
@@ -1,9 +1,6 @@
 package xyz.kxmischesdomi.picodeck;
 
 import com.vaadin.flow.theme.lumo.Lumo;
-import xyz.kxmischesdomi.picodeck.backend.legacy.BackendService;
-import xyz.kxmischesdomi.picodeck.backend.legacy.StreamDeck;
-import com.fazecast.jSerialComm.SerialPort;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.ServiceInitEvent;
 import com.vaadin.flow.server.VaadinServiceInitListener;
@@ -32,29 +29,6 @@ public class Application implements AppShellConfigurator, VaadinServiceInitListe
 
 	@Override
 	public void serviceInit(ServiceInitEvent serviceInitEvent) {
-
-//		try {
-//			BackendService.setInstance(new BackendService());
-//
-//			System.out.println("--------------------------------------------");
-//			System.out.println("List COM ports");
-//			System.out.println("--------------------------------------------");
-//			SerialPort[] comPorts = SerialPort.getCommPorts();
-//			for (int i = 0; i < comPorts.length; i++) {
-//				System.out.println("comPorts[" + i + "] = " + comPorts[i].getDescriptivePortName());
-//			}
-//			System.out.println("--------------------------------------------");
-//
-//			StreamDeck deck = new StreamDeck(comPorts[3]);
-//			deck.init();
-//			BackendService.getInstance().getDeviceHolder().getDevices().add(deck);
-//
-//			SpringApplication.getShutdownHandlers().add(() -> {
-//				BackendService.getInstance().onTerminate();
-//			});
-//		} catch (Exception exception) {
-//			exception.printStackTrace();
-//		}
 
 	}
 
